814230e943875e6766c7a85dbf9915e4a265924b
Cleyson Lima <cleysonph@gmail.com>
2024-10-09 23:58:31 -0300

    05:21 - Arrays

---
+++ Arrays.ps1
@@ -0,0 +1,57 @@
+﻿# Arrays
+
+# Criando array
+$frutas = @("Maçã", "Banana", "Pera", "Uva")
+Write-Host $frutas
+
+Write-Host ""
+
+# Acessando elementos do array
+Write-Host $frutas[0]
+Write-Host $frutas[3]
+Write-Host $frutas[1, 3, 2]
+
+Write-Host ""
+
+# Alterar os elementos do array
+$frutas[0] = "Morango"
+Write-Host $frutas
+
+Write-Host ""
+
+# Adicionando elementos no array
+$frutas += "Melancia"
+Write-Host $frutas
+
+Write-Host ""
+
+# Iterando sobre o array
+foreach ($fruta in $frutas) {
+    Write-Host $fruta
+}
+
+Write-Host ""
+
+# Verifando se um elemento existe no array
+if ($frutas -contains "Pera") {
+    Write-Host "Pera existe no array"
+} else {
+    Write-Host "Pera não existe no array"
+}
+
+Write-Host ""
+
+# Verificando o tamanho de um array
+Write-Host $frutas.Length
+
+Write-Host ""
+
+# Convertendo array em string
+$frutasString = $frutas -join ", "
+Write-Host $frutasString
+
+Write-Host ""
+
+# Convertendo string em array
+$frutasArray = $frutasString -split ", "
+Write-Host $frutasArray
---
+++ HashTables.ps1
@@ -0,0 +1,84 @@
+﻿# HashTables
+
+# Criando HashTable
+$usuario = @{
+    "nome" = "João"
+    "idade" = 30
+    "email" = "joao@mail.com"
+    "ativo" = $true
+}
+
+# Acessando elementos da hashtable
+Write-Host $usuario["nome"]
+Write-Host $usuario["email"]
+Write-Host $usuario["nome", "ativo", "email"]
+
+Write-Host $usuario.nome
+Write-Host $usuario.email
+
+Write-Host ""
+
+# Alterar elementos da hashtable
+$usuario["nome"] = "Maria"
+Write-Host $usuario["nome"]
+
+$usuario.nome = "José"
+Write-Host $usuario["nome"]
+
+Write-Host ""
+
+# Adicionando elementos na hashtable
+$usuario["sobrenome"] = "Silva"
+Write-Host $usuario["sobrenome"]
+
+$usuario.endereco = "Rua 1, 123"
+Write-Host $usuario["endereco"]
+
+Write-Host ""
+
+# Removendo elementos da hashtable
+$usuario.Remove("endereco")
+Write-Host $usuario.endereco
+
+Write-Host ""
+
+# Verificar se uma chave existe na hashtable
+if ($usuario.ContainsKey("endereco")) {
+    Write-Host "Chave 'endereco' existe na hashtable"
+} else {
+    Write-Host "Chave 'endereco' não existe na hashtable"
+}
+
+Write-Host ""
+
+# Verificar se um valor existe na hashtable
+if ($usuario.ContainsValue("joao@mail.com")) {
+    Write-Host "Valor 'joao@mail.com' existe na hashtable"
+} else {
+    Write-Host "Valor 'joao@mail.com' não existe na hashtable"
+}
+
+Write-Host ""
+
+# Iterando sobre a hashtable
+foreach ($chave in $usuario.Keys) {
+    Write-Host "${chave}: $($usuario[$chave])"
+}
+
+Write-Host ""
+
+# Obtendo apenas as chaves da hashtable
+$chaves = $usuario.Keys
+Write-Host $chaves
+
+Write-Host ""
+
+# Obtendo apenas os valores da hashtable
+$valores = $usuario.Values
+Write-Host $valores
+
+Write-Host ""
+
+# Clonando hashtable
+$usuario2 = $usuario.Clone()
+Write-Host $usuario2.nome
