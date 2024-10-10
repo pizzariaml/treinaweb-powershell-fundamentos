b1fb94b619a563d4bf698d2060ebc415cd3fb473
Cleyson Lima <cleysonph@gmail.com>
2024-10-10 00:45:31 -0300

    05:23 - Funções

---
+++ Funcoes.ps1
@@ -0,0 +1,21 @@
+﻿# Funções
+
+# Criar uma função
+function Saudacao {
+    param ([string]$nome = "visitante")
+    Write-Host "Olá, $nome"
+}
+
+function Soma{
+    param (
+        [int]$a,
+        [int]$b
+    )
+    return $a + $b
+}
+
+Saudacao -nome "Elton"
+Saudacao
+
+$resultado = Soma -a 10 -b 5
+Write-Host $resultado
