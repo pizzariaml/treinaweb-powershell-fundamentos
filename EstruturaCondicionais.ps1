e6faa301bd69cbbe209756c2ca794f4ade832a85
Cleyson Lima <cleysonph@gmail.com>
2024-10-09 23:32:13 -0300

    05:19 - Estruturas de condição

---
+++ EstruturaCondicionais.ps1
@@ -0,0 +1,13 @@
+﻿# Estruturas Condicionais
+
+$idade = Read-Host "Digite a sua idade"
+
+if ($idade -ge 18) {
+    Write-Host "Você é maior de idade"
+} elseif ($idade -ge 16) {
+    Write-Host "Você é menor de idade, mas já pode votar"
+} else {
+    Write-Host "Você é menor de idade"
+}
+
+Write-Host "Fim do script"
