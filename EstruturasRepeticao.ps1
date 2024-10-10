e0970493516a8a1adeeae8123def9dcbf7d500bb
Cleyson Lima <cleysonph@gmail.com>
2024-10-09 23:39:15 -0300

    05:20 - Estruturas de repetição

---
+++ EstruturasRepeticao.ps1
@@ -0,0 +1,29 @@
+﻿# Estruturas de Repetição
+
+# For
+for ($i = 1; $i -le 10; $i++) {
+    Write-Host "Número $i"
+}
+
+# Foreach
+$nomes = @("Cleyson", "Ana", "Elton", "Amauri", "Juliana")
+
+foreach ($nome in $nomes) {
+    Write-Host "Olá, $nome"
+}
+
+# While
+$contador = 1
+
+while ($contador -le 10) {
+    Write-Host "Número $contador"
+    $contador++
+}
+
+# Do While
+$contador = 11
+
+do {
+    Write-Host "Número $contador"
+    $contador++
+} while ($contador -le 10)
